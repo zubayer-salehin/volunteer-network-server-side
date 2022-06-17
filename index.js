394991eedaab24092ca1f93cd62224b327938aa0
zubayer-salehin <freelancerzubear@gmail.com>
2022-06-17 19:59:19 +0600

    server problame try to fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require("cors")
 const app = express()
-const port = 5000
+const port = Process.env.PORT || 5000;
 require("dotenv").config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
